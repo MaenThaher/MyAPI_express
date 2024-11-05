14c74ea9d3473af8994bdaacaa35fdfc0a9c7c00
maen <maentcefe@gmial.com>
2024-11-05 11:24:29 +0200

    add register_js.js and approach finish login

--- views/register_js.js
+++ views/register_js.js
@@ -1,4 +1,4 @@
-function check_login(u,p){
+function check_reg(f,e,u,p,c,bd){
     // fetch('http://localhost:3000/api/users/onuser/login') // Replace '/api/data' with your actual endpoint
     // .then(response => {
     //     if (!response.ok) {
@@ -16,33 +16,39 @@ function check_login(u,p){
     //     console.error('Fetch error:', error); // Handle errors
     // });
 //////////
-if(u == "" || p == ""){
-    alert("Please fill all fields")
+if(p != c){
+    alert("please confirm your password")
     return ;
 }
 
-const loginData = {
+const regData = {
+    fullname:f,
+    email:e,
     username:u,
-    password:p
+    password:p,
+    BirthDate:bd,
+
 }
 
+alert("fullname: "+regData.fullname)
+alert("email: "+regData.email)
+alert("username: "+regData.username)
+alert("password: "+regData.password)
+alert("BirthDate: "+regData.BirthDate)
 
-alert("username: "+loginData.username)
-alert("password: "+loginData.password)
 
-
-fetch('http://localhost:3000/api/users/onuser/login',
+fetch('http://localhost:3000/api/users/onuser',
     {method:'POST',
     headers:{
     'Content-Type': 'application/json',
-},body:JSON.stringify(loginData)
+},body:JSON.stringify(regData)
 }) // Replace '/api/data' with your actual endpoint
 .then(response => {
 
     if (!response.ok) {
         throw new Error('Network response was not ok ' + response.statusText);
     }
-//  alert(""+response.text())
+    //  alert(""+response.text())
  return response.text()
     //       return response.json(); // Parse JSON response
 
