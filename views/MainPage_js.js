1971058c7def6d0ef9e1b40871e86ee6c09641d7
maen <maentcefe@gmial.com>
2024-11-06 22:24:12 +0200

    add images file

---
+++ views/MainPage_js.js
@@ -0,0 +1,50 @@
+// Script to toggle the navbar on mobile
+const hamburger = document.getElementById('hamb');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+
+
+
+
+// Get elements for the slider and buttons
+const slides = document.querySelectorAll('.slide');
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+
+let currentSlide = 0;
+const totalSlides = slides.length;
+
+// Function to move to the next slide
+function goToSlide(slideIndex) {
+  const slider = document.querySelector('.slider');
+  // Ensure slideIndex stays within the range
+  if (slideIndex < 0) {
+    currentSlide = totalSlides - 1;
+  } else if (slideIndex >= totalSlides) {
+    currentSlide = 0;
+  } else {
+    currentSlide = slideIndex;
+  }
+  
+  slider.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+// Function for the "next" button
+nextButton.addEventListener('click', () => {
+  goToSlide(currentSlide + 1);
+});
+
+// Function for the "previous" button
+prevButton.addEventListener('click', () => {
+  goToSlide(currentSlide - 1);
+});
+
+// Auto-slide every 3 seconds
+setInterval(() => {
+  goToSlide(currentSlide + 1);
+}, 3000);
+
